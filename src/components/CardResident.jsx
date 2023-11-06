c4ca483e13fe83d5f843b44352623a0dcbb651dc
litdevfullstac <diego57mastodonte@gmail.comm>
2023-11-06 15:24:56 -0500

    6 commit

--- src/components/CardResident.jsx
+++ src/components/CardResident.jsx
@@ -16,15 +16,15 @@ const CardResident = ({ url }) => {
                 <div className="resident__status">
                     <div className={`resident__status__circle ${resident?.status}`}></div>
                     <span className="resident__status-value">{resident?.status}</span>
-                </div>
+                </div> 
             </header>
             <article className="resident__body">
                 <h3 className="resident__name">{resident?.name}</h3>
                 <hr  className="resident__separator"/>
               <ul className="resident__stats">
-              <li><span>Specie: </span><span>{resident?.species}</span></li>
-              <li><span>Origin: </span><span>{resident?.origin.name}</span></li>
-              <li><span>Eppisodes where appear: </span><span>{resident?.episode.length}</span></li>
+              <li><span className="resident__stat__item">Specie: </span><span className="resident__stat__label">{resident?.species}</span></li>
+              <li><span className="resident__stat__item">Origin: </span><span className="resident__stat__label">{resident?.origin.name}</span></li>
+              <li><span className="resident__stat__item">Eppisodes where appear: </span><span className="resident__stat__label">{resident?.episode.length}</span></li>
               </ul>
             </article>
         </section>
